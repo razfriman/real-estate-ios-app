76366fe66e2de96e4752f6ae752ea19df2d228a7
Raz Friman <superraz375@yahoo.com>
2015-09-14 08:33:36 -0500

    Create REST API manager using SwiftyJSON. Properties page loads random users from randomuser.me. Created nicer buttons for main actions.

---
+++ testiosapp1/RestApiManager.swift
@@ -0,0 +1,53 @@
+//
+//  RestApiManager.swift
+//  testiosapp1
+//
+//  Created by Raz Friman on 9/14/15.
+//  Copyright (c) 2015 Raz Friman. All rights reserved.
+//
+
+import Foundation
+
+typealias ServiceResponse = (JSON, NSError?) -> Void
+
+class RestApiManager: NSObject {
+    static let sharedInstance = RestApiManager()
+    
+    let baseURL = "http://api.randomuser.me/"
+    
+    func getRandomUser(onCompletion: (JSON) -> Void) {
+        let route = baseURL
+        makeHTTPGetRequest(route, onCompletion: { json, err in
+            onCompletion(json as JSON)
+        })
+    }
+    
+    func makeHTTPGetRequest(path: String, onCompletion: ServiceResponse) {
+        let request = NSMutableURLRequest(URL: NSURL(string: path)!)
+        
+        let session = NSURLSession.sharedSession()
+        
+        let task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
+            let json:JSON = JSON(data: data)
+            onCompletion(json, error)
+        })
+        task.resume()
+    }
+    
+    func makeHTTPPostRequest(path: String, body: [String: AnyObject], onCompletion: ServiceResponse) {
+        var err: NSError?
+        let request = NSMutableURLRequest(URL: NSURL(string: path)!)
+        
+        request.HTTPMethod = "POST"
+        
+        // Set the POST body for the request
+        request.HTTPBody = NSJSONSerialization.dataWithJSONObject(body, options: nil, error: &err)
+        let session = NSURLSession.sharedSession()
+        
+        let task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
+            let json:JSON = JSON(data: data)
+            onCompletion(json, err)
+        })
+        task.resume()
+    }
+}
---
+++ testiosapp1/testiosapp1/PropertiesViewController.swift
@@ -0,0 +1,71 @@
+//
+//  PropertiesViewController.swift
+//  testiosapp1
+//
+//  Created by Raz Friman on 9/14/15.
+//  Copyright (c) 2015 Raz Friman. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PropertiesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var tableView:UITableView?
+    var items = NSMutableArray()
+    
+    override func viewWillAppear(animated: Bool) {
+
+        let frame:CGRect = CGRect(x: 0, y: 100, width: self.view.frame.width, height: self.view.frame.height-100)
+        self.tableView = UITableView(frame: frame)
+        self.tableView?.dataSource = self
+        self.tableView?.delegate = self
+        self.view.addSubview(self.tableView!)
+        
+        let btn = UIButton(frame: CGRect(x: 0, y: 25, width: self.view.frame.width, height: 50))
+        btn.backgroundColor = UIColor.cyanColor()
+        btn.setTitle("Add new Dummy", forState: UIControlState.Normal)
+        btn.addTarget(self, action: "addDummyData", forControlEvents: UIControlEvents.TouchUpInside)
+        self.view.addSubview(btn)
+    }
+    
+    func addDummyData() {
+        RestApiManager.sharedInstance.getRandomUser { json in
+            let results = json["results"]
+            
+            for (index: String, subJson: JSON) in results {
+                
+                let user: AnyObject = subJson["user"].object
+                
+                self.items.addObject(user)
+                
+                dispatch_async(dispatch_get_main_queue(),{
+                    tableView?.reloadData()
+                })
+            }
+        }
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("CELL") as? UITableViewCell
+        
+        if cell == nil {
+            cell = UITableViewCell(style: UITableViewCellStyle.Value1, reuseIdentifier: "CELL")
+        }
+        
+        let user:JSON =  JSON(self.items[indexPath.row])
+        
+        let picURL = user["picture"]["medium"].string
+        let url = NSURL(string: picURL!)
+        let data = NSData(contentsOfURL: url!)
+        
+        cell!.textLabel?.text = user["username"].string
+        cell?.imageView?.image = UIImage(data: data!)
+        
+        return cell!
+    }
+}
