c02c3caad19888adf90ecc08dae07a10d30e0da2
Raz Friman <superraz375@yahoo.com>
2015-09-14 16:45:29 -0500

    authentication works

--- testiosapp1/testiosapp1/LoginViewController.swift
+++ testiosapp1/testiosapp1/LoginViewController.swift
@@ -9,11 +9,11 @@
 import UIKit
 
 class LoginViewController: UIViewController, UITextFieldDelegate {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-
+        
         emailTextField.delegate = self
         passwordTextField.delegate = self
     }
@@ -22,35 +22,29 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     @IBAction func loginClicked(sender: AnyObject) {
         // TODO - Login here
         
-
+        
         let parameters = [
-            "email": "user@user.com",
-            "password": "password"
+            "email": emailTextField.text!,
+            "password": passwordTextField.text!
         ]
         
-        
-//        request(.GET, "https://nodetest999.herokuapp.com/api").responseJSON { _, _, result in
-            //print(result)
-          //  debugPrint(result)
-        //}
-        
         request(.POST, "https://nodetest999.herokuapp.com/api/login", parameters: parameters, encoding: .JSON)
+            .validate()
             .responseString { _, _, result in
                 
-                print(result)
-                debugPrint(result)
-                
-                // Success???
-                self.performSegueWithIdentifier("loginSegue", sender: self)
-                
-                
+                switch(result) {
+                case .Success:
+                    print("Validation Successful")
+                    debugPrint(result)
+                    self.performSegueWithIdentifier("loginSegue", sender: self)
+                case .Failure(_, let error):
+                    print(error)
+                }
         }
-        
-
     }
     
     func textFieldShouldReturn(textField: UITextField) -> Bool {
@@ -73,7 +67,7 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         self.view.endEditing(true)
     }
     
-
+    
     @IBOutlet weak var emailTextField: UITextField!
     
     @IBOutlet weak var passwordTextField: UITextField!
